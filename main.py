b1e50195a27e5bdf35896a6d7e5e17921cb87f4c
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2021-08-03 10:55:49 +0530

    day before prep

--- main.py
+++ main.py
@@ -14,35 +14,35 @@ from data_base import DataBase
 
 print('Press \'q\' key to quit anytime')
 path = os.getcwd()
-directory= os.listdir(path+'\\images\\unknown\\')
+directory = os.listdir(path+'\\images\\unknown\\')
 conn = sqlite3.connect('data.db',check_same_thread=False)
 cur = conn.cursor()
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 global updating_name
 
-class Face:
+class Face():
 
-    def __init__(self,known_face_encodings,known_face_names,file_name=''):
+    def __init__(self, known_face_encodings, known_face_names, file_name=''):
         self.known_face_encodings = known_face_encodings
         self.known_face_names = known_face_names
         self.file_name = file_name
 
     # it stores all images and returns boolean value
-    def face_det(self,frame):
+    def face_det(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
         count = 0
-        bool = False
+        boolean = False
         for (x, y, w, h) in faces:
             crop = gray[y:y + h, x:x + w]
             cv2.imwrite(f'images/unknown/frame{count}.jpg', crop)
             count += 1
-            bool = True
+            boolean = True
 
-        return bool
+        return boolean
 
     #takes string of face location and returns name as a string
-    def face_rec(self,file_name):
+    def face_rec(self, file_name):
         unknown_image = face_recognition.load_image_file(file_name)
 
         face_locations = face_recognition.face_locations(unknown_image)
@@ -51,12 +51,12 @@ class Face:
         name = "Unknown"
 
         for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-         matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
 
-         face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-         best_match_index = np.argmin(face_distances)
-         if matches[best_match_index]:
-             name = self.known_face_names[best_match_index]
+        face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = self.known_face_names[best_match_index]
         return name
 
 
@@ -74,10 +74,12 @@ def qr_det(frame):
         date_time_obj = parser.parse(decoded_data)
 
         if date_time_obj > present:
-            print('QR Code detected Door Unlocking!')
+            print('valid QR Code is detected')
             bool_val = True
         else:
-            print('QR Code detected is expired')
+            print('detected QR Code is expired')
+            time.sleep(2)
+            os.system('cls')
 
     except:
         pass
@@ -98,7 +100,7 @@ def qr_face():
             door = True
             person_name = "QR Code"
             db_obj.update_log(person_name)
-            print('Door is now unlocked for 5s!')
+            print('Door is now unlocked!')
             time.sleep(5)
 
         if face_obj.face_det(frame):
@@ -106,13 +108,19 @@ def qr_face():
                 person_name = face_obj.face_rec('images/unknown/' + unknown_images)
 
                 if person_name != "Unknown":
-                    print(f"{person_name} detected")
+                    print(f"\n\n{person_name} detected")
                     door = True
                     db_obj.update_log(person_name)
                     print('Door is now unlocked for 5s!')
                     time.sleep(5)
-                    os.remove(path + '\\images\\unknown\\' + unknown_images)
+                    for images in directory:
+                        os.remove(path + '\\images\\unknown\\' + images)
+                    time.sleep(3)
+
+                    os.system('cls')
                     break
+                else:
+                    print('detected person is not in database')
 
 
         if door == True:
@@ -132,6 +140,7 @@ def face_encode(name_list,image_list):
     print("Face Encoding done!")
     return [known_face,known_image]
 
+
 if __name__ == "__main__":
 
     #Create database if it is not present
@@ -149,11 +158,48 @@ if __name__ == "__main__":
     print('Opening Camera.....')
     video = cv2.VideoCapture(0)
     print('Starting.......')
-
+    os.system('cls')
     #logic for detecting face
     while True:
         try:
-            qr_face()
+            door = False
+            while (door == False):
+
+                _, frame = video.read()
+                # frame = imutils.resize(frame, width=400)
+
+                bool_val = qr_det(frame)
+                if bool_val:
+                    door = True
+                    person_name = "QR Code"
+                    db_obj.update_log(person_name)
+                    print('Door is now unlocked!')
+                    time.sleep(5)
+
+                if face_obj.face_det(frame):
+                    for unknown_images in directory:
+                        person_name = face_obj.face_rec('images/unknown/' + unknown_images)
+
+                        if person_name != "Unknown":
+                            print(f"\n\n{person_name} detected")
+                            door = True
+                            db_obj.update_log(person_name)
+                            print('Door is now unlocked for 5s!')
+                            time.sleep(5)
+                            for images in directory:
+                                os.remove(path + '\\images\\unknown\\' + images)
+                            time.sleep(3)
+
+                            os.system('cls')
+                            break
+                        else:
+                            print('detected person is not in database')
+
+                if door == True:
+                    # db_obj.update_log(person_name)
+                    # print('Door is now unlocked for 5s!')
+                    # time.sleep(5)
+                    door = False
         except:
             pass
 
